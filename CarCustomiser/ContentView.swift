d6c9b90ccff54f9c0e7841a6fe61177552fb5d3d
Dylan Guo <guo.z@etoncollege.org.uk>
2023-01-13 21:51:40 +0000

    Added 4 customisable options for each car

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,11 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    let car = Car(make:"Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar: Int = 0
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    @State private var turboBoost = false
+    @State private var extraWeight = false
     
     var body: some View {
-        Text(car.displayStats())
-            .padding()
+        let exhaustPackageBinding = Binding<Bool> (
+            get: { self.exhaustPackage },
+            set: { newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 10
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 10
+                }
+            }
+        )
+        let tiresPackageBinding = Binding<Bool> (
+            get: { self.tiresPackage },
+            set: { newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                }
+            }
+        )
+        let turboBoostBinding = Binding<Bool> (
+            get: { self.turboBoost },
+            set: { newValue in
+                self.turboBoost = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration -= 5
+                    starterCars.cars[selectedCar].handling -= 1
+                } else {
+                    starterCars.cars[selectedCar].acceleration += 5
+                    starterCars.cars[selectedCar].handling += 1
+                }
+            }
+        )
+        let extraWeightBinding = Binding<Bool> (
+            get: { self.extraWeight },
+            set: { newValue in
+                self.extraWeight = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration += 3
+                    starterCars.cars[selectedCar].topSpeed += 15
+                } else {
+                    starterCars.cars[selectedCar].acceleration -= 3
+                    starterCars.cars[selectedCar].topSpeed -= 15
+                }
+            }
+        )
+        Form {
+            VStack(alignment: .leading, spacing: 20) {
+                Text(starterCars.cars[selectedCar].displayStats())
+                Button("Next Car", action: {
+                    selectedCar += 1
+                    if selectedCar == starterCars.cars.count{
+                        selectedCar = 0
+                    }
+                })
+            }
+            Section {
+                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                Toggle("Tires Package", isOn: tiresPackageBinding)
+                Toggle("Turbo Boost", isOn: turboBoostBinding)
+                Toggle("Extra Weight", isOn: extraWeightBinding)
+            }
+        }
     }
 }
 
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,15 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Guo, Dylan (Coll) on 13/01/2023.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make:"Mazda", model: "MX-5", topSpeed: 125, acceleration: 11.7, handling: 5),
+                Car(make:"Volkswagen", model: "Golf", topSpeed: 115, acceleration: 14.2, handling: 7),
+                Car(make:"Mercedes-Benz", model: "G-Class", topSpeed: 140, acceleration: 9.6, handling: 3),
+                Car(make:"Lamborghini", model: "Urus Performante", topSpeed: 190, acceleration: 7.3, handling: 8)]
+}
